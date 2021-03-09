ee128bbd735fe64d2b0dcec77405551657374b52
ravishastri9 <ravishastri9031@gmail.com>
2021-03-09 17:32:35 +0530

    First Commit

---
+++ 01. Tip Calculator/Day1.py
@@ -0,0 +1,28 @@
+# debugging error practice
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello "+" World")')
+print("New lines can be created with a backslash and n.")
+
+# use of input and len function
+
+print( len( input("What is your name? ") ) )
+
+# use of Variables
+a = input("a:")
+b = input("b:")
+c = a
+a = b
+b = c
+print("a = "+a)
+print("b =" +b)
+
+# Final project (Implement what you have learned today)
+
+print("Welcome to band name generator")
+city_name = input("what's the name of the city you grew up in \n ")
+pet_name = input("what's your pet name \n")
+print("Your brand name could be " + city_name+ " " +pet_name)
+
+
