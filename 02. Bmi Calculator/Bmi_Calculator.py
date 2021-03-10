70732dc5bb72b9ffc19448ea28b3bc439931c91b
ravishastri9 <ravishastri9031@gmail.com>
2021-03-10 16:10:16 +0530

    First Commit

--- 02. Bmi Calculator/Bmi_Calculator.py
+++ 02. Bmi Calculator/Bmi_Calculator.py
@@ -1,3 +1,5 @@
+"Program helps you the find the bmi of particular person"
+
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
 bmi = float(weight)/float(height)**2
